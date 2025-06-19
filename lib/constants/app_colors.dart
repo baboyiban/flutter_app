2dd88fd9fe3dd33f6da064bfdf8b29cfd4c78da0
최다루한 <chl11wq12@gmail.com>
2025-06-19 14:18:51 +0900

    fix: Color 동기화

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color blue = Color(0xFFBFDFFF);
+  static const Color blue = Color(0xFF99CCFF);
   static const Color gray = Color(0xFFEFEFEF);
   static const Color deepGray = Color(0xFFDFDFDF);
-  static const Color red = Color(0xFFFFBFBF);
+  static const Color red = Color(0xFFFF9999);
   static const Color text = Color(0xFF000000);
-  static const Color yellow = Color(0xFFFFEFBF);
+  static const Color yellow = Color(0xFFFFE699);
   static const Color green = Color(0xFFBFFFBF);
   static const Color black = Color(0xFF000000);
   static const Color shadow = Color(0x80000000);
